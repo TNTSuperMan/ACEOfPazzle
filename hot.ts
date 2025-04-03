63cac61d8adc2f6c52650c69dabe8b73fb6fc2ec
TNTSuperMan <abworld@outlook.jp>
2025-04-03 18:06:24 +0900

    bge-binがあるので消す

--- hot.ts
+++
@@ -1,7 +0,0 @@
-import { $ } from "bun";
-import { resolve } from "path";
-
-const run = (): unknown =>
-    $`bun ${resolve(__dirname, "hot-program.ts")}`
-        .then(()=>console.log("Hard reload!")).then(run);
-run();
